0c89444cf2d434d72949aea286e4b1a6a89729da
ankurbhargava0511 <ankurbhargava0511@gmail.com>
2019-10-14 01:28:23 +0530

    react: routing

--- Learning/Practice/React/first-app/src/App.js
+++ Learning/Practice/React/first-app/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-
 import HomePage from "./components/HomePageComponents/HomePage";
 import AboutPage from "./components/AboutPageComponents/AboutPage";
 import Header from "./components/CommonComponents/Header";
+import { Route } from "react-router-dom";
 
 // Old Way of routing
 // export default function App() {
@@ -11,18 +11,30 @@ import Header from "./components/CommonComponents/Header";
 //   return <HomePage />;
 // }
 
-export default function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
+// export default function App() {
+//   //old way part 2
+//     function getPage() {
+//       const route = window.location.pathname;
+//       if (route === "/about") return <AboutPage />;
+//       return <HomePage />;
+//     }
 
-  // functions are refered with {} brackets and just name is required
+//   // functions are refered with {} brackets and just name is required. this is how we write Js within JSX
+//   return (
+//     <div className="container-fluid">
+//       <Header />
+//       {getPage()}
+//     </div>
+//   );
+// }
+// beacuse home page route match all route so use exact
+
+export default function App() {
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
     </div>
   );
 }
--- Learning/Practice/React/first-app/src/components/CommonComponents/Header.js
+++ Learning/Practice/React/first-app/src/components/CommonComponents/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+// anchor here will reload the page do use Link
+// link will enable client side routing
+// anchor the oldway
+// function Header() {
+//   return (
+//     <nav>
+//       <a href="/">Home</a> | <a href="/about">About</a>
+//     </nav>
+//   );
+// }
+
 function Header() {
+  const activeStyle = { color: "orange" };
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/about">About</a>
+      <NavLink exact to="/" activeStyle={activeStyle}>
+        Home
+      </NavLink>{" "}
+      |{" "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
     </nav>
   );
 }
